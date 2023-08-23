81181ab1953fa461a0feffbc7415d98aa31a802a
AnujRuhela7 <ruhela7777@gmail.com>
2023-08-23 19:16:31 +0530

    Added buttons for various components

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,51 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+const HomeScreen = ({ navigation }) => {
+
+  return (
+    <View>
+      <Text style={ styles.text }>My First App </Text>
+      <Button
+        style={ styles.buttonSize }
+        onPress={ () => navigation.navigate("Components") }
+        title="Go To Components Demo"
+      />
+      <Button
+        style={ styles.buttonSize }
+        onPress={ () => navigation.navigate("List") }
+        title="Go To List Demo"
+      />
+      <Button
+        style={ styles.buttonSize }
+        onPress={ () => navigation.navigate("Images") }
+        title="Go To Images" />
+      <Button
+        style={ styles.buttonSize }
+        onPress={ () => navigation.navigate("Counter") }
+        title="Counter Application"
+      />
+      <Button
+        style={ styles.buttonSize }
+        onPress={ () => navigation.navigate("Colours") }
+        title="Coloured Tiles Demo"
+      />
+      <Button
+        style={ styles.buttonSize }
+        onPress={ () => navigation.navigate("SquareColourTile") }
+        title="Custom Colour Tile Demo"
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30,
+    fontSize: 30, marginVertical: 15
   },
+  buttonSize: {
+    fontSize: 15, marginVertical: 15
+  }
 });
 
 export default HomeScreen;
